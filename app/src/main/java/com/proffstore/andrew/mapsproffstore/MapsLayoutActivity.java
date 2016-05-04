772f40be0c61888c6087e13fca502e34c53a491d
Andrew Kovtun <crosshp.ak@gmail.com>
2016-05-05 02:48:40 +0300

    AlertDialog

--- app/src/main/java/com/proffstore/andrew/mapsproffstore/MapsLayoutActivity.java
+++ app/src/main/java/com/proffstore/andrew/mapsproffstore/MapsLayoutActivity.java
@@ -1,32 +1,36 @@
 package com.proffstore.andrew.mapsproffstore;
 
-import android.annotation.TargetApi;
+import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.NotificationCompat;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.android.SphericalUtil;
+import com.mikepenz.materialdrawer.DrawerBuilder;
 
 public class MapsLayoutActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -36,38 +40,40 @@ public class MapsLayoutActivity extends FragmentActivity implements OnMapReadyCa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maps_layout);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, getResources().getStringArray(R.array.map_types));
-        spinner.setAdapter(spinnerAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (mMap != null) {
-                    switch (position) {
-                        case 0: {
-                            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                            break;
-                        }
-                        case 1: {
-                            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                            break;
-                        }
-                        case 2: {
-                            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                            break;
-                        }
-                    }
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
         // Initialize google map
         SupportMapFragment mapFragment = (SupportMapFragment) this.getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MapsLayoutActivity.this, R.style.AppCompatAlertDialogStyle);
+                builder.setTitle(getResources().getString(R.string.display_mode));
+                View view = View.inflate(MapsLayoutActivity.this,R.layout.menu_point_layout, null);
+                builder.setView(view);
+                builder.setPositiveButton(getResources().getString(R.string.show), null);
+                builder.setNegativeButton(getResources().getString(R.string.cancel), null);
+                builder.show();
+                final ListView pointsList = (ListView) view.findViewById(R.id.listView);
+                final LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.routesMenuContent);
+                RadioButton radioButtonPoints = (RadioButton) view.findViewById(R.id.radioButtonPoints);
+                radioButtonPoints.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if(isChecked){
+                            pointsList.setVisibility(View.VISIBLE);
+                            linearLayout.setVisibility(View.GONE);
+                        } else {
+                            pointsList.setVisibility(View.GONE);
+                            linearLayout.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
+            }
+        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        new DrawerBuilder().withActivity(this).withToolbar(toolbar).build();
+
     }
 
     // Override volume buttons
@@ -98,7 +104,7 @@ public class MapsLayoutActivity extends FragmentActivity implements OnMapReadyCa
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        mMap.getUiSettings().setZoomControlsEnabled(true);
+        //   mMap.getUiSettings().setZoomControlsEnabled(true);
         // Add a marker in Sydney and move the camera
         sydney = new LatLng(-34, 151);
         LatLng sydney1 = new LatLng(-35, 152);
@@ -108,11 +114,21 @@ public class MapsLayoutActivity extends FragmentActivity implements OnMapReadyCa
         mMap.addMarker(new MarkerOptions().position(sydney1).title("Marker in new Sydney"));
         mMap.addMarker(new MarkerOptions().position(sydney1).title("Marker in new Sydney"));
         mMap.addPolyline(new PolylineOptions().add(sydney).add(sydney1).add(sydney2));
-        circle = new CircleOptions().center(sydney).radius(5000).visible(true).fillColor(ContextCompat.getColor(this, R.color.colorMarker)).strokeColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        circle = new CircleOptions().strokeWidth(3).center(sydney).radius(5000).visible(true).fillColor(ContextCompat.getColor(this, R.color.colorMarker)).strokeColor(ContextCompat.getColor(this, R.color.colorMarkerCorner));
         mMap.addCircle(circle);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         Toast.makeText(getApplicationContext(), String.valueOf(SphericalUtil.computeDistanceBetween(sydney, sydney1) <= circle.getRadius()), Toast.LENGTH_SHORT).show();
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
@@ -125,7 +141,8 @@ public class MapsLayoutActivity extends FragmentActivity implements OnMapReadyCa
                         .setContentInfo("Info")
                         .setContentText("Content text")
                         .setContentIntent(pendingIntent);
-                NotificationManagerCompat.from(getBaseContext()).notify(1,builder.build());
+                NotificationManagerCompat.from(getBaseContext()).notify(1, builder.build());
+
             }
         });
     }
