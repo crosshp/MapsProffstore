0893738a739217b1350bfd93506e0a228df38db4
Andrew Kovtun <crosshp.ak@gmail.com>
2016-05-05 17:17:42 +0300

    Calendar

--- app/src/main/java/com/proffstore/andrew/mapsproffstore/MapsLayoutActivity.java
+++ app/src/main/java/com/proffstore/andrew/mapsproffstore/MapsLayoutActivity.java
@@ -1,6 +1,7 @@
 package com.proffstore.andrew.mapsproffstore;
 
 import android.Manifest;
+import android.app.DialogFragment;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,11 +12,14 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioButton;
@@ -30,9 +34,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.android.SphericalUtil;
+import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
-public class MapsLayoutActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Calendar;
+
+public class MapsLayoutActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -40,27 +48,60 @@ public class MapsLayoutActivity extends FragmentActivity implements OnMapReadyCa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maps_layout);
+
+        float x = getIntent().getFloatExtra("x", 0);
+        float y = getIntent().getFloatExtra("y", 0);
+        if (x != 0 && y != 0) {
+            LatLng intentLatLng = new LatLng(x, y);
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(intentLatLng));
+        }
         // Initialize google map
         SupportMapFragment mapFragment = (SupportMapFragment) this.getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MapsLayoutActivity.this, R.style.AppCompatAlertDialogStyle);
                 builder.setTitle(getResources().getString(R.string.display_mode));
-                View view = View.inflate(MapsLayoutActivity.this,R.layout.menu_point_layout, null);
+                View view = View.inflate(MapsLayoutActivity.this, R.layout.menu_point_layout, null);
                 builder.setView(view);
                 builder.setPositiveButton(getResources().getString(R.string.show), null);
                 builder.setNegativeButton(getResources().getString(R.string.cancel), null);
-                builder.show();
+                final AlertDialog alertDialog = builder.show();
                 final ListView pointsList = (ListView) view.findViewById(R.id.listView);
                 final LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.routesMenuContent);
+                EditText editShowStart = (EditText) view.findViewById(R.id.editShowStart);
+                final EditText editShowFinish = (EditText) view.findViewById(R.id.editShowFinish);
+                editShowFinish.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View v, boolean hasFocus) {
+                        if (hasFocus) {
+                            Log.e("calendar", "I am in Focus");
+                            Calendar now = Calendar.getInstance();
+                            DatePickerDialog dpd = DatePickerDialog.newInstance(
+                                    new DatePickerDialog.OnDateSetListener() {
+                                        @Override
+                                        public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+                                            editShowFinish.setText(dayOfMonth+"/"+monthOfYear+"/"+year);
+                                        }
+                                    },
+                                    now.get(Calendar.YEAR),
+                                    now.get(Calendar.MONTH),
+                                    now.get(Calendar.DAY_OF_MONTH)
+                            );
+                            dpd.show(getFragmentManager(), "Datepickerdialog");
+                        } else {
+                            Log.e("calendar", "NO Focus");
+                        }
+                    }
+                });
                 RadioButton radioButtonPoints = (RadioButton) view.findViewById(R.id.radioButtonPoints);
                 radioButtonPoints.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if(isChecked){
+                        if (isChecked) {
                             pointsList.setVisibility(View.VISIBLE);
                             linearLayout.setVisibility(View.GONE);
                         } else {
@@ -71,9 +112,20 @@ public class MapsLayoutActivity extends FragmentActivity implements OnMapReadyCa
                 });
             }
         });
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        new DrawerBuilder().withActivity(this).withToolbar(toolbar).build();
-
+        final Drawer drawer = new DrawerBuilder().withActivity(this).build();
+        ImageButton imageButton = (ImageButton) findViewById(R.id.drawerButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawer != null) {
+                    if (!drawer.isDrawerOpen()) {
+                        drawer.openDrawer();
+                    } else {
+                        drawer.closeDrawer();
+                    }
+                }
+            }
+        });
     }
 
     // Override volume buttons
@@ -136,6 +188,8 @@ public class MapsLayoutActivity extends FragmentActivity implements OnMapReadyCa
                 Toast.makeText(getApplicationContext(), String.valueOf(SphericalUtil.computeDistanceBetween(sydney, latLng) <= circle.getRadius()), Toast.LENGTH_SHORT).show();
                 NotificationCompat.Builder builder = new NotificationCompat.Builder(getBaseContext());
                 Intent intent = new Intent(getApplication(), MapsLayoutActivity.class);
+                intent.putExtra("x", latLng.latitude);
+                intent.putExtra("y", latLng.longitude);
                 PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 1, intent, 0);
                 builder.setSmallIcon(R.drawable.common_plus_signin_btn_icon_light)
                         .setContentInfo("Info")
